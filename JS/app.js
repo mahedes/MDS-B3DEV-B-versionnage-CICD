961c5c856e42e20a55767b6b7eaa1ee2525327e1
Axel Dion <axel77dion@gmail.com>
2025-11-03 14:42:30 +0100

    addTexteToDoList

---
+++ JS/app.js
@@ -0,0 +1,41 @@
+let todos = [];
+
+document.querySelector('.new-todo').onkeypress = function(e) {
+	if (e.key === 'Enter' && this.value) {
+		todos.push({ text: this.value, done: false });
+		this.value = '';
+		show();
+	}
+};
+
+function show() {
+	const list = document.querySelector('.todo-list');
+	list.innerHTML = '';
+	
+	todos.forEach((todo, i) => {
+		list.innerHTML += `
+			<li class="${todo.done ? 'completed' : ''}">
+				<div class="view">
+					<input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''} onchange="toggle(${i})">
+					<label>${todo.text}</label>
+					<button class="destroy" onclick="remove(${i})"></button>
+				</div>
+			</li>
+		`;
+	});
+	
+	const left = todos.filter(t => !t.done).length;
+	document.querySelector('.todo-count').innerHTML = `<strong>${left}</strong> left`;
+}
+
+function toggle(i) {
+	todos[i].done = !todos[i].done;
+	show();
+}
+
+function remove(i) {
+	todos.splice(i, 1);
+	show();
+}
+
+show();
