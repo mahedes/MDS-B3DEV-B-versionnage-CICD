204b141199f339c43c87aa9e90c73323186d8fb8
Nicolas Moulin <nicolas.moulin104@gmail.com>
2025-11-05 15:15:57 +0100

    ajout d'une fonctionnalité date/heure lorsqu'on crée une todolist

--- JS/app.js
+++ JS/app.js
@@ -1,41 +1,52 @@
 let todos = [];
 
-document.querySelector('.new-todo').onkeypress = function(e) {
-	if (e.key === 'Enter' && this.value) {
-		todos.push({ text: this.value, done: false });
-		this.value = '';
-		show();
-	}
+document.querySelector(".new-todo").onkeypress = function (e) {
+  if (e.key === "Enter" && this.value) {
+    todos.push({ text: this.value, done: false, createdAt: new Date() });
+    this.value = "";
+    show();
+  }
 };
 
 function show() {
-	const list = document.querySelector('.todo-list');
-	list.innerHTML = '';
-	
-	todos.forEach((todo, i) => {
-		list.innerHTML += `
-			<li class="${todo.done ? 'completed' : ''}">
+  const list = document.querySelector(".todo-list");
+  list.innerHTML = "";
+
+  todos.forEach((todo, i) => {
+    list.innerHTML += `
+			<li class="${todo.done ? "completed" : ""}">
 				<div class="view">
-					<input class="toggle" type="checkbox" ${todo.done ? 'checked' : ''} onchange="toggle(${i})">
-					<label>${todo.text}</label>
+					<input class="toggle" type="checkbox" ${
+            todo.done ? "checked" : ""
+          } onchange="toggle(${i})">
+					<label>
+					${todo.text}
+					<small style ="display: block; font-size:12px; color:#888;">
+					(créé le ${new Date(todo.createdAt).toLocaleDateString()} à ${new Date(
+      todo.createdAt
+    ).toLocaleTimeString()})
+					</small>
+					</label>
 					<button class="destroy" onclick="remove(${i})"></button>
 				</div>
 			</li>
 		`;
-	});
-	
-	const left = todos.filter(t => !t.done).length;
-	document.querySelector('.todo-count').innerHTML = `<strong>${left}</strong> left`;
+  });
+
+  const left = todos.filter((t) => !t.done).length;
+  document.querySelector(
+    ".todo-count"
+  ).innerHTML = `<strong>${left}</strong> left`;
 }
 
 function toggle(i) {
-	todos[i].done = !todos[i].done;
-	show();
+  todos[i].done = !todos[i].done;
+  show();
 }
 
 function remove(i) {
-	todos.splice(i, 1);
-	show();
+  todos.splice(i, 1);
+  show();
 }
 
 show();
